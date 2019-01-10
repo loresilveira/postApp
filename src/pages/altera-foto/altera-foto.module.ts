442d431e16b4bcfe1095383a02ba2dda752c44e7
Santos <lorena.santos@capgemini.com>
2019-01-10 15:24:17 -0300

    Layouts das páginas AlteraFoto e DetPost

---
+++ src/pages/altera-foto/altera-foto.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { AlteraFotoPage } from './altera-foto';
+
+@NgModule({
+  declarations: [
+    AlteraFotoPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(AlteraFotoPage),
+  ],
+})
+export class AlteraFotoPageModule {}
---
+++ src/pages/det-post/det-post.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { DetPostPage } from './det-post';
+
+@NgModule({
+  declarations: [
+    DetPostPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(DetPostPage),
+  ],
+})
+export class DetPostPageModule {}
