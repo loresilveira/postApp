45ec08b239ca9efe2c08e9c239d64a3947d54e69
Santos <lorena.santos@capgemini.com>
2019-01-10 10:06:32 -0300

    Layout da página Mensagens

--- src/pages/mensagens/mensagens.ts
+++ src/pages/mensagens/mensagens.ts
@@ -12,14 +12,30 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-mensagens',
   templateUrl: 'mensagens.html',
+  
 })
 export class MensagensPage {
+
+  public listaMsg: Array<string>=['1 -batman', '2-superman', '3- black panter'];
+  
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MensagensPage');
+    
+  }
+
+  getItems(ev: any){
+    let val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.listaMsg = this.listaMsg.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
   }
 
 }
