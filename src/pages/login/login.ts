d15b7b836c419eb069e3346d37a1dfeaa93e1f48
Santos <lorena.santos@capgemini.com>
2019-01-17 13:45:51 -0300

    Botão habilitado com mínimo de carcteres

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { PerfilPage } from '../perfil/perfil';
-
-
+import { FormBuilder, Validators } from '@angular/forms';
+import { Keyboard } from '@ionic-native/keyboard';
 /**
  * Generated class for the LoginPage page.
  *
@@ -17,17 +17,72 @@ import { PerfilPage } from '../perfil/perfil';
 })
 export class LoginPage {
   
+  public loginForm: any;
 
+  nome='';
+  senha='';
+
+  usuario = {
+    "id":"",
+    "nome":"",
+    "senha": ""
+  }
+
+  messageNome = ""
+  messagePassword = "";
+  errorNome = false;
+  errorPassword = false;
+  disabled = true;
+ 
   constructor(public navCtrl: NavController,
-              public navParams: NavParams) {
+              public navParams: NavParams,
+              formBuilder: FormBuilder) {
+              /**
+               this.loginForm = formBuilder.group({
+                  nome: ['', Validators.compose([Validators.minLength(3), Validators.maxLength(20),
+                    Validators.required])],
+                  senha: ['', Validators.compose([Validators.minLength(4), Validators.maxLength(20),
+                  Validators.required])],
+                });
+               */
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+    
+      
   }
 
   goToPerfilPage(){
-    this.navCtrl.push(PerfilPage);
+    this.navCtrl.push(PerfilPage); 
   }
 
+ /** 
+  login() {
+    let { email, password } = this.loginForm.controls;
+
+    if (!this.loginForm.valid) {
+      if (!email.valid) {
+        this.errorNome = true;
+        this.messageNome = "O usuário precisa ter de 3 a 20 caracteres";
+      } else {
+        this.messageNome = "";
+      }
+
+      if (!password.valid) {
+        this.errorPassword = true;
+        this.messagePassword ="A senha precisa ter de 4 a 20 caracteres";
+      } else {
+        this.disabled = false;
+        this.messagePassword = "";
+        
+      }
+    }
+    else {
+      alert("Login Realizado");
+    }
+  }
+*/ 
+ 
 }
