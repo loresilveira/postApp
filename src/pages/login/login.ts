608cb91361da218aac7e540324464c4c81c1f62b
Santos <lorena.santos@capgemini.com>
2019-01-21 11:19:18 -0300

    Senha criptografada

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { PerfilPage } from '../perfil/perfil';
 import { AuthProvider } from '../../providers/auth/auth';
+import hasha from 'hasha';
 
 
 /**
@@ -19,12 +20,14 @@ import { AuthProvider } from '../../providers/auth/auth';
 export class LoginPage {
 
   usuario = {
-    "id": "",
+  
     "nome": "",
     "senha": ""
   }
-  
- 
+
+   
+  private cripto:string;
+
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
     public authProvider: AuthProvider) {
@@ -34,51 +37,23 @@ export class LoginPage {
     console.log('ionViewDidLoad LoginPage');
 
   }
-
+  
   salvaUsuario(){
-
-    /*const hasha = (this.usuario.senha, [SHA_256,Base64]) => {
-      SHA_256 = SHA_256 || {};
     
-      let outputEncoding = opts.encoding || 'hex';
-    
-      if (outputEncoding === 'buffer') {
-        outputEncoding = undefined;
-      }
-    
-      const hash = crypto.createHash(opts.algorithm || 'sha512');
-    
-      const update = buf => {
-        const inputEncoding = typeof buf === 'string' ? 'utf8' : undefined;
-        hash.update(buf, inputEncoding);
-      };
-    
-      if (Array.isArray(input)) {
-        input.forEach(update);
-      } else {
-        update(input);
-      }
-    
-      return hash.digest(outputEncoding);
-    };
-    console.log(hasha);
-    
-    /** .then(hash => {
-      console.log(hash);
-    });   */
-
-    /**
-     * this.authProvider.postUsuario(this.usuario).then((result) => {
-      console.log(result);
-    }, (err) => {
+    this.cripto = hasha(this.usuario.senha,{algorithm:'sha256',encoding:'base64'});
+    this.authProvider.postUsuario({login:this.usuario.nome,senha:this.cripto}).then((result) => {
+    console.log( result);
+    this.navCtrl.setRoot(PerfilPage, {'usuario':result});
+    }) .catch((err) => {
       console.log(err);
     });
-     */
+     
+    
 
   }
  
   goToPerfilPage() {
-    this.navCtrl.push(PerfilPage);
+    
     
   }
 
